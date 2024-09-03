2f827877ee4184eb5aafc3cd9c4e2f8d8f74d550
imHeeshan <imHeeshan@gmail.com>
2024-09-03 11:12:05 +0530

    completed side navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,11 +9,45 @@ function openNav() {
   }
 
   //when window resize collapse function trigger
-  window.addEventListener('resize', function () {
+  // window.addEventListener('resize', function () {
 
-  var collpase_section = document.getElementById("collapse-nav");
-    if (window.innerWidth > 600 &&   collpase_section.style.display === "none") {
-      collpase_section.style.display = "flex";
+  // var collpase_section = document.getElementById("collapse-nav");
+  //   if (window.innerWidth > 600 &&   collpase_section.style.display === "none") {
+  //     collpase_section.style.display = "flex";
+  //   }
+  // });
+  
+  // select side nav
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var side_nav = document.getElementById("side-nav");
+    
+    if (side_nav) {  
+      var nav_lists = side_nav.getElementsByClassName("side-nav-list");
+      
+      for (var i = 0; i < nav_lists.length; i++) {
+        nav_lists[i].addEventListener("click", function() {
+          var current = document.getElementsByClassName("active");
+          
+          if (current.length > 0) {
+            current[0].className = current[0].className.replace(" active", "");
+          }
+          
+          this.className += " active";
+        });
+      }
+    } else {
+      console.error("Element with id 'side' not found.");
     }
   });
-  
+  function is_toggle () {
+    var collapse_button = document.getElementById("side-nav");
+    var top_logo_section = document.getElementById("logo-section");
+    
+    if (collapse_button) {  
+      
+      collapse_button.classList.toggle("isShow");
+      top_logo_section.classList.toggle("isShow");
+    
+    } 
+  }
